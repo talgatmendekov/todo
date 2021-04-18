e6af07a8a408840b2dfb17522a0e356edff93f7c
Talgat Mendekov <talgatmendekov>
2021-04-18 19:41:33 +0600

    init

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def homepage(request):
+    return HttpResponse ('hello world')
+
+def test(request):
+    return render(request, 'test.html')
+
+def go(request):
+    return render(request, 'go.html')
